56d5959b75ceb29c1f7a5dd4eb2c4c2f59bbf5c4
İsmail Tezal <ismailtezal33@gmail.com>
2024-01-24 12:01:49 +0300

    fix: refactored card & result component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,15 +15,26 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ data }) => {
     return (
         <>
-            {data.map((user: User) => (
-                <div className='flex items-center  space-x-4 ring bg-blue-400 px-4 py-3 rounded-lg shadow-md' key={user.id}>
-                    <img src={user.avatar} width={48} alt={`${user.first_name} ${user.last_name}`} className='rounded-full ' />
-                    <div className='flex flex-col justify-center'>
-                        <p className='text-white font-semibold text-lg'>{`${user.first_name} ${user.last_name}`}</p>
-                        <p className='text-gray-200'>{user.email}</p>
+            {data.length > 0 ? (
+                data.map((user: User) => (
+                    <div className='flex items-center space-x-4 ring bg-blue-400 px-4 py-3 rounded-lg shadow-md' key={user.id}>
+                        <img
+                            src={user.avatar}
+                            width={48}
+                            alt={`${user.first_name} ${user.last_name}`}
+                            className='rounded-full flex-shrink-0 object-cover w-12 h-12'
+                        />
+                        <div className='flex-grow flex flex-col justify-center'>
+                            <p className='text-white font-semibold text-lg'>{`${user.first_name} ${user.last_name}`}</p>
+                            <p className='text-gray-200 text-sm md:text-base drop-shadow'>{user.email}</p>
+                        </div>
                     </div>
+                ))
+            ) : (
+                <div className='flex justify-center'>
+                <p className="text-white text-2xl">No user found with this id.</p>
                 </div>
-            ))}
+            )}
         </>
     );
 };
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,74 +1,95 @@
-import { useEffect, useState } from "react"
-import { useRouter } from "next/router"
-import Card from "./Card"
-import PaginationControl from "./PaginationControl"
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Card from './Card';
+import PaginationControl from './PaginationControl';
+import SearchInput from './SearchInput';
 
 interface User {
-    id: number
-    email: string
-    first_name: string
-    last_name: string
-    avatar: string
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
 }
 
 interface ApiResponse {
-    page: number
-    per_page: number
-    total: number
-    total_pages: number
-    data: User[]
-    support: {
-        url: string
-        text: string
-    }
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
+  support: {
+    url: string;
+    text: string;
+  };
 }
 
 const Result = () => {
-    const router = useRouter()
-    const { query } = router
-    const [data, setData] = useState<User[]>([])
-    const [currentPage, setCurrentPage] = useState<number>(1)
+  const router = useRouter();
+  const { query } = router;
+  const [originalData, setOriginalData] = useState<User[]>([]);
+  const [data, setData] = useState<User[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [searchMessage, setSearchMessage] = useState<string>('');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const page = query['page'] ?? '1'
-            const per_page = query['per_page'] ?? '5'
-            const url = `https://reqres.in/api/users?page=${page}&per_page=${per_page}`
+  useEffect(() => {
+    const fetchData = async () => {
+      const page = query['page'] ?? '1';
+      const per_page = query['per_page'] ?? '5';
+      const url = `https://reqres.in/api/users?page=${page}&per_page=${per_page}`;
 
-            try {
-                const response = await fetch(url)
-                const jsonData: ApiResponse = await response.json()
-                setData(jsonData.data)
-            } catch (error) {
-                console.error("Error fetching data:", error)
-            }
-        }
+      try {
+        const response = await fetch(url);
+        const jsonData: ApiResponse = await response.json();
+        setOriginalData(jsonData.data);
+        setData(jsonData.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-        fetchData()
-    }, [query])
+    fetchData();
+  }, [query]);
 
-    const handlePreviousPage = () => {
-        if (currentPage > 1) {
-            const newPage = currentPage - 1
-            router.push(`/?page=${newPage}`)
-            setCurrentPage(newPage)
-        }
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      const newPage = currentPage - 1;
+      router.push(`/?page=${newPage}`);
+      setCurrentPage(newPage);
     }
+  };
 
-    const handleNextPage = () => {
-        if (currentPage < data.length) {
-            const newPage = currentPage + 1
-            router.push(`/?page=${newPage}`)
-            setCurrentPage(newPage)
-        }
+  const handleNextPage = () => {
+    if (currentPage < data.length) {
+      const newPage = currentPage + 1;
+      router.push(`/?page=${newPage}`);
+      setCurrentPage(newPage);
     }
+  };
 
-    return (
-        <div className="flex flex-col gap-2 bg-blue-500 m-4 p-4 rounded-md shadow-md">
-            <Card data={data} />
-            <PaginationControl currentPage={currentPage} handleNextPage={handleNextPage} handlePreviousPage={handlePreviousPage} />
-        </div>
-    )
-}
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value;
+    setSearchInput(searchValue);
+
+    if (searchValue === '') {
+      setData(originalData);
+      setSearchMessage('');
+    } else {
+      const user = originalData.find((user) => user.id.toString() === searchValue);
+      setData(user ? [user] : []);
+      setSearchMessage(user ? '' : 'No user with this ID found.');
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-2 bg-blue-500 m-4 p-4 rounded-md shadow-md">
+      <Card data={data} />
+      <PaginationControl currentPage={currentPage} handleNextPage={handleNextPage} handlePreviousPage={handlePreviousPage} />
+      <SearchInput handleSearch={handleSearch} searchInput={searchInput} />
+    </div>
+  );
+};
 
 export default Result;
