eca5254e7009282df10368b8881bd7b3a40ed8f6
ismailtezal <ismailtezal33@gmail.com>
2024-02-08 15:47:41 +0300

    fix: Refactor code with prettier

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -2,19 +2,22 @@ import { PageResponse } from "@/types/PageResponse";
 import { User } from "@/types/User";
 
 export interface ApiServiceResult {
-    data: User[];
-    total: number;
+  data: User[];
+  total: number;
 }
 
-export const fetchData = async (page: number, perPage: number): Promise<ApiServiceResult> => {
-    const url = `https://reqres.in/api/users?page=${page}&per_page=${perPage}`;
+export const fetchData = async (
+  page: number,
+  perPage: number
+): Promise<ApiServiceResult> => {
+  const url = `https://reqres.in/api/users?page=${page}&per_page=${perPage}`;
 
-    try {
-        const response = await fetch(url);
-        const jsonData: PageResponse = await response.json();
-        return { data: jsonData.data, total: jsonData.total };
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
+  try {
+    const response = await fetch(url);
+    const jsonData: PageResponse = await response.json();
+    return { data: jsonData.data, total: jsonData.total };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
